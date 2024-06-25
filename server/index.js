52b41113ef9686507112f0d91bb01452a76f529c
tamarahegyi <hegyitamara0206@gmail.com>
2024-06-25 14:01:22 +0400

    final push

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const app = express();
@@ -21,4 +21,36 @@ connect();
 // Start server
 app.listen(3000, () => {
   console.log(`Server running on port ${PORT}`);
+});*/
+
+const express = require('express');
+const cors = require('cors');
+const connectToMongo = require('./db');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 8181;
+
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+
+// Connect to MongoDB
+connectToMongo();
+
+// Routes
+app.use('/api/auth', require('./routes/auth'));
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+  // Start the server
+app.listen(PORT, () => {
+console.log(`Server is running on port http://localhost:${PORT}`);
 });
--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const API_URL = 'http://localhost:3000/SignUp';
+export const API_URL = window.location.origin;
